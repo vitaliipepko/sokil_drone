db4671bf644486c0618d8fb7ceaefc135a09f727
Vitalii Pepko <vitaliipepko@gmail.com>
2023-01-30 20:41:58 +0200

    fix errors

--- src/sokil_drone_controller/src/pid.cpp
+++ src/sokil_drone_controller/src/pid.cpp
@@ -24,37 +24,37 @@ PID::PID(
     {
     }
 
-    PID::reset()
+PID::reset()
+{
+    integral_ = 0;
+    previous_error_ = 0;
+    previous_time = rosTime::now();
+}
+
+PID::setIntegral(float integral)
+{
+    integral_ = integral;
+}
+
+PID::update(float value, float target_value)
+{
+    ros::Time time{ros::Time::now()};
+    float dt{time.toSec() - previous_time_.toSec()};
+    float error{target_value - value}};
+    integral_ += error * dt;
+    integral_ = std::max(std::min(integral_, integrator_max_), integrator_min_);
+    float p{k_p_ * error};
+    float d{0};
+
+    if(dt > 0)
     {
-        integral_ = 0;
-        previous_error_ = 0;
-        previous_time = rosTime::now();
+        d = k_d_ * (error - previous_error_) / dt;
     }
 
-    PID::setIntegral(float integral)
-    {
-        integral_ = integral;
-    }
+    float i{k_i_ * integral_};
+    float output{p + i + d};
+    previous_error = error;
+    previous_time = time;
 
-    PID::update(float value, float target_value)
-    {
-        ros::Time time{ros::Time::now()};
-        float dt{time.toSec() - previous_time_.toSec();
-        float error{target_value - value}};
-        integral_ += error * dt;
-        integral_ = std::max(std::min(integral_, integrator_max_), integrator_min_);
-        float p{k_p_ * error};
-        float d{0};
-
-        if(dt > 0)
-        {
-            d = k_d_ * (error - previous_error_) / dt;
-        }
-
-        float i{k_i_ * integral_};
-        float output{p + i + d};
-        previous_error = error;
-        previous_time = time;
-
-        return srd::max(std::min(output, max_output_), min_output_);
-    }
+    return std::max(std::min(output, max_output_), min_output_);
+}
